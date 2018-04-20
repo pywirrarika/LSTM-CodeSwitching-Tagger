06d6217d3aacd0e9b27c01ce82489a915ae676f9
Jesus <pywirrarika@gmail.com>
2018-04-19 22:36:11 -0500

    LSTM with dropout

--- train.py
+++ train.py
@@ -14,7 +14,6 @@ BIDIRECTIONAL = True
 NUM_DIRS = 2 if BIDIRECTIONAL else 1
 WEIGHT_DECAY = 1e-4
 SAVE_EVERY = 1
-DROPOUT = .3
 
 EMBEDDING_DIM = 100
 HIDDEN_DIM = 100
@@ -27,18 +26,26 @@ class LSTMTagger(nn.Module):
         self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
 
         #Creamos un LSTM con tamaño de entrada de embedding y estados de salida ocultos hidden_dim
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim // NUM_DIRS, 
+                dropout = DROPOUT, 
+                num_layers = NUM_LAYERS, 
+                bidirectional = BIDIRECTIONAL,
+                )
 
         self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
         self.hidden = self.init_hidden()
+        self.dropout = nn.Dropout(p=DROPOUT)
     
     def init_hidden(self):
-        return (autograd.Variable(torch.zeros(1,1,self.hidden_dim)),
-                autograd.Variable(torch.zeros(1,1,self.hidden_dim)))
+        return (autograd.Variable(torch.zeros(NUM_LAYERS * NUM_DIRS,1,self.hidden_dim // NUM_DIRS )),
+                autograd.Variable(torch.zeros(NUM_LAYERS * NUM_DIRS,1,self.hidden_dim // NUM_DIRS)))
 
     def forward(self,sentence):
         embeds = self.word_embeddings(sentence)
+
+        embeds = self.dropout(embeds) 
         lstm_out, self.hidden = self.lstm(embeds.view(len(sentence), 1, -1), self.hidden)
+        lstm_out = self.dropout(lstm_out) 
         tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
         tag_scores = F.log_softmax(tag_space, dim=1)
         return tag_scores
@@ -57,7 +64,7 @@ def train():
     optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE)
 
 
-    filename = "epoch"
+    filename = "models/model"
     #Train
     print('Train with', len(data), 'examples.')
     for epoch in range(300):
